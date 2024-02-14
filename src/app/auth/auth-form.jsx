976b59cac736893fb506afbb051751a6b190fe17
Jason <jason@arielles-mbp.mynetworksettings.com>
2024-02-14 13:39:10 -0500

    update supabase url

--- src/app/auth/auth-form.jsx
+++ src/app/auth/auth-form.jsx
@@ -5,7 +5,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { COLOR_THEME_COOKIE_NAME } from '../constants'
 
 export default function AuthForm() {
-  const supabase = createClient(process.env.PROJECT_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
   const darkMode = Cookies.get(COLOR_THEME_COOKIE_NAME) === 'light' ? 'default' : 'dark'
   const URL = process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL;
   return (
