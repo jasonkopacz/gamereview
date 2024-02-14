8ef1238cbc0c8fb8c6afc68600dbaef0dfd57138
Jason <jason@arielles-mbp.mynetworksettings.com>
2024-02-14 18:55:49 -0500

    testing hardcode apikey

--- src/app/auth/auth-form.jsx
+++ src/app/auth/auth-form.jsx
@@ -4,7 +4,7 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 export default function AuthForm({darkMode}) {
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsenV4aG1sa2NqdXFsa3NkeWdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MzY0NzcsImV4cCI6MjAyMDQxMjQ3N30.H3nQysecR34LwAcEfmvp9CVxWFMZkSvzEgDgaYkGAMQ")
   const URL = process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL;
   return (
     <>
