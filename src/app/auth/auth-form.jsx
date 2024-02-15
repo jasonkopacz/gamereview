e0f1f7aa591d3af38e66c65d813e7b97ba47aaeb
Jason <jason@arielles-mbp.mynetworksettings.com>
2024-02-14 19:36:25 -0500

    another

--- src/app/auth/auth-form.jsx
+++ src/app/auth/auth-form.jsx
@@ -4,7 +4,7 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 export default function AuthForm({darkMode}) {
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
   const URL = process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL;
   return (
     <>
