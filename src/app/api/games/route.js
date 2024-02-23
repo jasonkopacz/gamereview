96f051b82edea46240aaea7ff4a0dcfffc0ad860
Jason <jason@Arielles-MacBook-Pro.local>
2024-02-22 19:35:02 -0500

    update dashboard fetch

--- src/app/api/games/route.js
+++ src/app/api/games/route.js
@@ -1,15 +1,11 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
+import { supabase } from "@/app/database";
 
 export async function GET(req) {
-  const supabase = createServerComponentClient({ cookies });
-  if (req.method === 'GET') {
-    const { data, error } = await supabase
-    .from('games_old')
-    .select(`*`)
-    
-    return NextResponse.json({games: data}, { status: 200 })
+  if (req.method === "GET") {
+    const { data, error } = await supabase.from("games_old").select(`*`);
+
+    return NextResponse.json({ games: data }, { status: 200 });
   }
-  return NextResponse.json({error: error}, {status: 404 })
+  return NextResponse.json({ error: error }, { status: 404 });
 }
