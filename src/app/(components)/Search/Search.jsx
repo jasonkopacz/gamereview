62231b4facbfc6b2a21c4b18bf5be72072e66792
Jason <jason@Arielles-MacBook-Pro.local>
2024-02-23 23:14:08 -0500

    finished getting all games

--- src/app/(components)/Search/Search.jsx
+++ src/app/(components)/Search/Search.jsx
@@ -15,137 +15,41 @@ const searchClient = algoliasearch(
 
 const index = searchClient.initIndex("one");
 
-// await index.saveObjects(data).then(({ objectIDs }) => {
-//   console.log(objectIDs);
-// });
-// const { taskID } = await client.saveObject({
-//   indexName: "GAMES",
-//   data
-// });
-
-// console.log("Ready to search!");
-
 export function Hit({ hit }) {
   return (
     <article>
-      <Image src={hit.poster_path} alt={hit.title} height={300} width={300} />
-      <p>{hit.genres[0]}</p>
+      <Image
+        src={hit.background_image}
+        alt={hit.name}
+        height={300}
+        width={300}
+      />
+      {hit.genres.map((genre, i) => (
+        <p key={i}>{genre}</p>
+      ))}
+      {hit.tags.map((tag, i) => (
+        <p key={i}>{tag}</p>
+      ))}
+      {hit.platforms.map((platform, i) => (
+        <p key={i}>{platform}</p>
+      ))}
+      <p>{hit.released}</p>
+      <p>{hit.esrb_rating}</p>
+      <p>{hit.rating}</p>
+      <p>{hit.metacritic}</p>
       <Highlight attribute="title" hit={hit} />
       <p>${hit.release_date}</p>
     </article>
   );
 }
 
-export default async function Search() {
-  const data = await fetchData();
-  // index
-  //   .saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
-  //   .then(({ objectIds }) => {
-  //     console.log(objectIds);
-  //   });
-  // const requests = data.map((record) => {
-  //   return {
-  //     action: "addObject",
-  //     body: record
-  //   };
-  // });
-
-  // const { taskID } = await index.batch({
-  //   indexName: "one",
-  //   batchWriteParams: {
-  //     requests
-  //   }
-  // });
-
+export default function Search() {
   return (
-    <InstantSearch searchClient={searchClient} indexName="games">
+    <InstantSearch searchClient={searchClient} indexName="one">
       <RefinementList attribute="genres" />
+      <RefinementList attribute="tags" />
       <SearchBox />
       <Hits hitComponent={Hit} />
     </InstantSearch>
   );
 }
-
-const apiKey = "25c2bcabe0f84c32b4c1a41d66f18c9b";
-const baseUrl = "https://api.rawg.io/api";
-const endpoint = "/games";
-let page = 3001;
-let results = [];
-let formattedResults = [];
-
-export const fetchData = async () => {
-  try {
-    console.log(page);
-    const response = await fetch(
-      `${baseUrl}${endpoint}?key=${apiKey}&page=${page}&page_size=40`
-    );
-    let data = await response.json();
-    let formattedGames = data.results.map((game) => {
-      // console.log(game);
-      const genres = game.genres.flatMap((genre) => genre.name);
-      const platforms = game.platforms.flatMap(
-        (platform) => platform.platform.name
-      );
-      const screenshots = game.short_screenshots.flatMap(
-        (screenshot) => screenshot.image
-      );
-      const tags = game?.tags.flatMap((tag) => tag.name);
-      const slug = game?.slug;
-      const name = game?.name;
-      const released = game?.released;
-      const background_image = game?.background_image;
-      const rating = game?.rating;
-      const metacritic = game?.metacritic;
-      const esrb_rating = game?.esrb_rating?.name;
-      results.push({
-        name,
-        slug,
-        genres,
-        tags,
-        platforms,
-        released,
-        background_image,
-        screenshots,
-        esrb_rating,
-        rating,
-        metacritic
-      });
-      formattedResults.push({
-        name,
-        slug,
-        genres,
-        tags,
-        platforms,
-        released,
-        background_image,
-        screenshots,
-        esrb_rating,
-        rating,
-        metacritic
-      });
-    });
-    if (page % 100 === 0) {
-      console.log(results);
-      console.log(formattedResults);
-      index
-        .saveObjects(formattedResults, { autoGenerateObjectIDIfNotExist: true })
-        .then(({ objectIds }) => {
-          console.log(objectIds);
-        });
-      page++;
-      formattedResults = [];
-      fetchData();
-      // 21590
-    } else if (page < 6000) {
-      page++;
-      fetchData();
-    } else {
-      console.log(results);
-      return results;
-    }
-  } catch (error) {
-    page++;
-    fetchData();
-    console.error("Error fetching data:", error);
-  }
-};
