716835fceabf05f995f3625675d3ad31770f0ba1
Jason <jason@Arielles-MacBook-Pro.local>
2024-02-28 12:46:13 -0500

    webhook update

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -2,11 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { supabase } from "@/app/database";
 import { auth } from "@clerk/nextjs";
-import { buffer } from "micro";
-
 export async function POST(req) {
-  console.log("WEBHOOK");
-  const { getToken } = auth();
   const WEBHOOK_SECRET = process.env.WEBHOOK_SIGNING_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -16,9 +12,6 @@ export async function POST(req) {
   }
 
   const headerPayload = headers();
-  const headers = req.headers;
-  console.log(headerPayload);
-  console.log(headers);
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
@@ -28,24 +21,27 @@ export async function POST(req) {
       status: 400
     });
   }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
   const wh = new Webhook(WEBHOOK_SECRET);
+
   let evt;
+
   try {
-    evt = wh.verify(headerPayload, headers);
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature
+    });
   } catch (err) {
     console.error("Error verifying webhook:", err);
     return new Response("Error occured", {
       status: 400
     });
   }
-  console.log(evt);
 
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-  const token = await getToken({ template: "supabase" });
-  supabase.auth.setAuth(token);
-  Console.log("SUPABASE TIME");
-  console.log(payload);
   const { id, first_name, last_name, username, image_url } = payload.data;
   const full_name = first_name + "" + last_name;
   const email = payload.data.email_addresses[0].email_address;
