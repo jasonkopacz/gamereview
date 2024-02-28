6325cc00be501e586607209a477126adf36d39d1
Jason <jason@Arielles-MacBook-Pro.local>
2024-02-27 19:30:10 -0500

    remove middleware to test

--- middleware.js
+++ middleware.js
@@ -2,13 +2,13 @@ import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    if (auth.userId && req.nextUrl.pathname === "/") {
-      const url = req.nextUrl.clone();
-      url.pathname = "/dashboard";
-      return NextResponse.redirect(url);
-    }
-  },
+  // afterAuth(auth, req, evt) {
+  //   if (auth.userId && req.nextUrl.pathname === "/") {
+  //     const url = req.nextUrl.clone();
+  //     url.pathname = "/dashboard";
+  //     return NextResponse.redirect(url);
+  //   }
+  // },
   publicRoutes: ["/", "/sign-up", "/api/webhooks(.*)"]
 });
 
