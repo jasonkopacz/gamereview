f62a8688879f0cbcac98d99556764daca7185871
Jason <jason@Arielles-MacBook-Pro.local>
2024-02-27 19:36:13 -0500

    cookies in page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./page.module.css";
 import { COLOR_THEME_COOKIE_NAME } from "./constants";
 import { SignIn } from "@clerk/nextjs";
@@ -6,18 +5,18 @@ import { cookies } from "next/headers";
 import { dark, neobrutalism } from "@clerk/themes";
 
 export default function Home() {
-  const savedTheme = cookies().get(COLOR_THEME_COOKIE_NAME).value;
-  const clerkTheme = savedTheme === "light" ? neobrutalism : dark;
-  const color =
-    savedTheme === "dark" ? "hsl(242deg 100% 70%)" : "hsl(50deg 100% 70%)";
+  // const savedTheme = cookies().get(COLOR_THEME_COOKIE_NAME).value;
+  // const clerkTheme = savedTheme === "light" ? neobrutalism : dark;
+  // const color =
+  //   savedTheme === "dark" ? "hsl(242deg 100% 70%)" : "hsl(50deg 100% 70%)";
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <SignIn
           redirectUrl="/dashboard"
           appearance={{
-            baseTheme: clerkTheme,
-            variables: { colorPrimary: color }
+            baseTheme: dark,
+            variables: { colorPrimary: "hsl(242deg 100% 70%)" }
           }}
         />
         <h1>The Game Review</h1>
